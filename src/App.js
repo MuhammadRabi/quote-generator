961502b0a2ec00568369a309a558ee7d6926228a
Muhammad Rabi <vonrabi07@yahoo.com>
2023-04-28 17:14:10 +0300

    Add social share btns

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
+import {
+  TwitterShareButton,
+  WhatsappShareButton,
+  TwitterIcon,
+  WhatsappIcon,
+  TelegramShareButton,
+  TelegramIcon,
+} from "react-share";
 
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +40,34 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+          <div className="share-btns">
+            <TwitterShareButton
+              url={"https://github.com/MuhammadRabi/quote-generator"}
+              title={`"${quote.content}" - ${quote.author}`}
+            >
+              <TwitterIcon size={32} round={true} />
+            </TwitterShareButton>
+            <WhatsappShareButton
+              url={"https://github.com/MuhammadRabi/quote-generator"}
+              title={`"${quote.content}" - ${quote.author}`}
+            >
+              <WhatsappIcon size={32} round={true} />
+            </WhatsappShareButton>
+            <TelegramShareButton
+              url={"https://github.com/MuhammadRabi/quote-generator"}
+              title={`"${quote.content}" - ${quote.author}`}
+            >
+              <TelegramIcon size={32} round={true} />
+            </TelegramShareButton>
+          </div>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
